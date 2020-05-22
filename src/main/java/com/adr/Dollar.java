3f870675f3bba6ff0ea693f1bb04400a688e49cd
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-22 15:16:38 -0500

    Dollar class added, MoneyTest added

---
+++ src/main/java/com/adr/Dollar.java
@@ -0,0 +1,15 @@
+package com.adr;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times (int multiplier){
+        amount *=  multiplier;
+    }
+
+}
