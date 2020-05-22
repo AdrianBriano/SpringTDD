0d73107c0fde5c4cf1c878c2b33cabff37f67596
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-22 16:02:24 -0500

    Equals and hashCode methods added for testing object equality, amount attribute encapsulated

--- src/main/java/com/adr/Dollar.java
+++ src/main/java/com/adr/Dollar.java
@@ -1,15 +1,30 @@
 package com.adr;
 
+import java.util.Objects;
+
 public class Dollar {
 
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times (int multiplier){
-        amount *=  multiplier;
+    Dollar times (int multiplier){
+
+        return new Dollar( amount * multiplier);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
--- src/test/java/com/adr/MoneyTest.java
+++ src/test/java/com/adr/MoneyTest.java
@@ -3,15 +3,25 @@ package com.adr;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
 
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(new Dollar(10), product);
 
+        product = five.times(3);
+        assertEquals(new Dollar(15), product);
+
+    }
+
+    @Test
+    void testEqulity() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
