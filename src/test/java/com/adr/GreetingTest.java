e3516b3c9c658d4a74fd8b948a56695a1050e5ea
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-26 12:39:45 -0500

    Before all and after all examples added

--- src/test/java/com/adr/GreetingTest.java
+++ src/test/java/com/adr/GreetingTest.java
@@ -1,13 +1,17 @@
 package com.adr;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called once!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -25,4 +29,15 @@ class GreetingTest {
     void helloWorld1() {
         System.out.println(greeting.helloName("Adrian"));
     }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After - I am only called once!");
+    }
+
 }
