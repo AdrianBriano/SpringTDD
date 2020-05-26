daae2053fcf1700820c73c89035427e0dcf78a23
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-26 15:03:51 -0500

    Starting code

---
+++ src/main/java/com/adr/repositories/VisitRepository.java
@@ -0,0 +1,6 @@
+package com.adr.repositories;
+
+import com.adr.model.Visit;
+
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+}
---
+++ src/main/java/com/adr/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.adr.services;
+
+import com.adr.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
