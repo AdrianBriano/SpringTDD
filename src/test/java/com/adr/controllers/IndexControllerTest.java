b402ec793d17188f0f813c2045abd7b2a963f8ae
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-27 09:19:25 -0500

    AssertTimeout example

--- src/test/java/com/adr/controllers/IndexControllerTest.java
+++ src/test/java/com/adr/controllers/IndexControllerTest.java
@@ -1,8 +1,11 @@
 package com.adr.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +33,26 @@ class IndexControllerTest {
 
         assertThrows(ValueNotFoundException.class, () -> {
             controller.oopsHandler();
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I got here");
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeoutPremp() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+
+            System.out.println("I got here premp");
         });
     }
 }
