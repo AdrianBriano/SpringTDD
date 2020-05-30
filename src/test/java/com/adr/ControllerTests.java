414b23005654b4365a1bd1eed6162428d00be890
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-05-29 21:43:29 -0500

    Methods on unit tests interface

--- src/test/java/com/adr/ControllerTests.java
+++ src/test/java/com/adr/ControllerTests.java
@@ -1,9 +1,18 @@
 package com.adr;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(PER_CLASS)
 @Tag("controller")
 public interface ControllerTests {
 
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("Lets do something here");
+    }
 
 }
