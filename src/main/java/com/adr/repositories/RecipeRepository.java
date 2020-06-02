2dd72632f0ed2d544a27b638cccdf94631df2cea
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-06-02 15:58:22 -0500

    Initial project

---
+++ src/main/java/com/adr/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package com.adr.repositories;
+
+import com.adr.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
---
+++ src/main/java/com/adr/services/RecipeService.java
@@ -0,0 +1,13 @@
+package com.adr.services;
+
+import com.adr.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
