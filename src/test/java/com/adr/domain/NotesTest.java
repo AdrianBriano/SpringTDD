8e23c69c5767dde793bd03333df609d03b8af3a3
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-06-03 09:13:34 -0500

    pom updated with Surefire, failsafe, jUnit Vintage and added jUnit 5 test

---
+++ src/test/java/com/adr/domain/NotesTest.java
@@ -0,0 +1,14 @@
+package com.adr.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotesTest {
+
+    @Test
+    void getId() {
+
+        assertEquals(3,3);
+    }
+}
