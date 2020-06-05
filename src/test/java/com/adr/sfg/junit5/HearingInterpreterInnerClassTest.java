688029e3ff32294b7a74c7ae1a6ff8c0236a1952
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-06-05 14:12:29 -0500

    Using Inner Class Configuration for tests

---
+++ src/test/java/com/adr/sfg/junit5/HearingInterpreterInnerClassTest.java
@@ -0,0 +1,34 @@
+package com.adr.sfg.junit5;
+
+import com.adr.sfg.HearingInterpreter;
+import com.adr.sfg.HearingInterpreterTest;
+import com.adr.sfg.LaurelWordProducer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = HearingInterpreterInnerClassTest.TestConfig.class)
+class HearingInterpreterInnerClassTest {
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        HearingInterpreter hearingInterpreter(){
+            return new HearingInterpreter(new LaurelWordProducer());
+        }
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+
+        assertEquals(word, "Laurel");
+    }
+}
