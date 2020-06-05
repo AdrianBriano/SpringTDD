f7bef0fd094585ffaa9900179be15c456975165c
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-06-05 09:58:28 -0500

    Unit tests for VetController and ClinicServiceImp

---
+++ src/test/java/com/adr/web/VetControllerTest.java
@@ -0,0 +1,73 @@
+package com.adr.web;
+
+import com.adr.model.Vet;
+import com.adr.model.Vets;
+import com.adr.service.ClinicService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.anyOf;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    private static final String VETS_VET_LIST = "vets/vetList";
+
+    @Mock
+    ClinicService clinicService;
+    @Mock
+    Map<String, Object> modelMap;
+
+    @InjectMocks
+    VetController vetController;
+
+    List<Vet> vets = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        vets.add(new Vet());
+
+        given(clinicService.findVets()).willReturn(vets);
+    }
+
+    @Test
+    void showVetListTest() {
+        //when
+        String view = vetController.showVetList(modelMap);
+
+        //then
+        assertThat(view).isEqualToIgnoringCase(VETS_VET_LIST);
+
+        then(clinicService).should(times(1)).findVets();
+        then(clinicService).shouldHaveNoMoreInteractions();
+        then(modelMap).should().put(anyString(), any());
+    }
+
+    @Test
+    void showResourcesVetListTest() {
+        //when
+        Vets returnedVets = vetController.showResourcesVetList();
+
+        //then
+        assertThat(returnedVets).isInstanceOf(Vets.class);
+        assertThat(returnedVets.getVetList()).hasSize(1);
+
+        then(clinicService).should(times(1)).findVets();
+        then(clinicService).shouldHaveNoMoreInteractions();
+
+    }
+}
