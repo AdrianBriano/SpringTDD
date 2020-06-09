043058b56aab9d4245c5ecc91613c52756f6b08c
AdrianBrianoGarcia <adrian.briano.garcia@outlook.com>
2020-06-09 14:14:30 -0500

    Unit test with jsonPath validation

---
+++ src/test/java/com/adr/brewery/web/controllers/BeerControllerTest.java
@@ -0,0 +1,63 @@
+package com.adr.brewery.web.controllers;
+
+import com.adr.brewery.services.BeerService;
+import com.adr.brewery.web.model.BeerDto;
+import com.adr.brewery.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class BeerControllerTest {
+
+    @Mock
+    BeerService beerService;
+
+    @InjectMocks
+    BeerController beerController;
+
+    MockMvc mockMvc;
+
+    BeerDto beerDto;
+
+    @BeforeEach
+    void setUp() {
+        beerDto = BeerDto.builder().id(UUID.randomUUID())
+                .version(1)
+                .beerName("Beer2")
+                .beerStyle(BeerStyleEnum.PALE_ALE)
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(4)
+                .upc(123456789012L)
+                .createdDate(OffsetDateTime.now())
+                .build();
+
+        mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
+    }
+
+    @Test
+    void testGetBeerById() throws Exception {
+        given(beerService.findBeerById(any())).willReturn(beerDto);
+
+        mockMvc.perform(get("/api/v1/beer/" + beerDto.getId()))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.id", is(beerDto.getId().toString())));
+    }
+}
